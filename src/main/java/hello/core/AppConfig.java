69e23623820a847fc6c2dbdf27b72b6753865e2a
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-06-18 13:42:44 +0900

    [Feat/ # 12] 스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // 리팩토링 : 역할 = 메서드 명, 구현 = new로 생성한 객체
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); // 생성자 주입 -> MemberServiceImpl 클래스가 더이상 MemberRepository의 구현체에 의존하지 않음
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     private static DiscountPolicy discountPolicy() {
         // return new FIxDiscountPolicy();
         return new RateDiscountPolicy();
