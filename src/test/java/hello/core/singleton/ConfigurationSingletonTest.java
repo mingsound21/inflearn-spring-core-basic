de845165074f2b7e3b93cca8c6a7a78f916c98e0
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-07-10 02:18:33 +0900

    [Feat/ #25] @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -35,4 +35,19 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberServiceImpl.getMemberRepository()).isSameAs(memberRepository3);
         Assertions.assertThat(orderServiceImpl.getMemberRepository()).isSameAs(memberRepository3);
     }
+
+    @Test
+    void configurationDeep() throws Exception {
+        //given
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+
+        //when
+        System.out.println("bean.getClass() = " + bean.getClass());// bean.getClass() = class hello.core.AppConfig$$SpringCGLIB$$0
+        // 순수한 클래스인 경우 : class hello.core.AppConfig
+        // xxxCGLIB : 내가 만든 클래스가 아니라 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용해서 AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록. 임의의 다른 클래스가 싱글톤이 보장되도록해준다.
+
+        //then
+    }
 }
